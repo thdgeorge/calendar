671fe328ef87354b9fc33933e97c11fa03da69f9
George Theodorou <theodwrougewrgios@gmail.com>
2023-10-31 15:11:14 +0200

    add 2 HACKS

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-
-var express      = require('express');
-var path         = require('path');
-var favicon      = require('serve-favicon');
-var logger       = require('morgan');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var moment       = require('moment');
+var bodyParser = require('body-parser');
+var moment = require('moment');
 const createSessionMiddleware = require('./lib/middleware/withSession');
 
 var app = express();
 
 // View engine setup
-var handlebars = require('express-handlebars')
-  .create({
-    defaultLayout : 'main',
-    extname       : '.hbs',
-    helpers       : require('./lib/view/helpers')(),
-  });
+var handlebars = require('express-handlebars').create({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  helpers: require('./lib/view/helpers')(),
+});
 
 app.engine('.hbs', handlebars.engine);
 app.set('view engine', '.hbs');
@@ -33,74 +31,66 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
 // Setup authentication mechanism
 const passport = require('./lib/passport')();
 
-app.use(createSessionMiddleware({
-  sequelizeDb: app.get('db_model').sequelize,
-}))
+app.use(
+  createSessionMiddleware({
+    sequelizeDb: app.get('db_model').sequelize,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
-
-
 
 // Custom middlewares
 //
 // Make sure session and user objects are available in templates
-app.use(function(req,res,next){
-
+app.use(function (req, res, next) {
   // Get today given user's timezone
   var today;
 
-  if ( req.user && req.user.company ) {
+  if (req.user && req.user.company) {
     today = req.user.company.get_today();
   } else {
     today = moment.utc();
   }
 
-  res.locals.session     = req.session;
+  res.locals.session = req.session;
   res.locals.logged_user = req.user;
   res.locals.url_to_the_site_root = '/';
   res.locals.requested_path = req.originalUrl;
   // For book leave request modal
-  res.locals.booking_start = today,
-  res.locals.booking_end = today,
-  res.locals.keep_team_view_hidden =
-    !! (req.user && req.user.company.is_team_view_hidden && ! req.user.admin);
+  (res.locals.booking_start = today),
+    (res.locals.booking_end = today),
+    (res.locals.keep_team_view_hidden = !!(
+      req.user &&
+      req.user.company.is_team_view_hidden &&
+      !req.user.admin
+    ));
 
   next();
 });
 
-app.use(function(req,res,next){
-    res.locals.custom_java_script = [
-      '/js/bootstrap-datepicker.js',
-      '/js/global.js'
-    ];
-    res.locals.custom_css = [
-      '/css/bootstrap-datepicker3.standalone.css'
-    ];
+app.use(function (req, res, next) {
+  res.locals.custom_java_script = [
+    '/js/bootstrap-datepicker.js',
+    '/js/global.js',
+  ];
+  res.locals.custom_css = ['/css/bootstrap-datepicker3.standalone.css'];
 
-    next();
+  next();
 });
 
 // Enable flash messages within session
-app.use( require('./lib/middleware/flash_messages') );
+app.use(require('./lib/middleware/flash_messages'));
 
-app.use( require('./lib/middleware/session_aware_redirect') );
+app.use(require('./lib/middleware/session_aware_redirect'));
 
 // Here will be publicly accessible routes
 
-app.use(
-  '/feed/',
-  require('./lib/route/feed')
-);
+app.use('/feed/', require('./lib/route/feed'));
 
-app.use(
-  '/integration/v1/',
-  require('./lib/route/integration_api')(passport)
-);
+app.use('/integration/v1/', require('./lib/route/integration_api')(passport));
 
 app.use(
   '/',
@@ -112,15 +102,9 @@ app.use(
 
 app.use('/api/v1/', require('./lib/route/api'));
 
-app.use(
-  '/calendar/',
-  require('./lib/route/calendar')
-);
+app.use('/calendar/', require('./lib/route/calendar'));
 
-app.use(
-  '/settings/',
-  require('./lib/route/settings')
-);
+app.use('/settings/', require('./lib/route/settings'));
 
 // '/settings/' path is quite big hence there are two modules providing handlers for it
 app.use('/settings/', require('./lib/route/departments'));
@@ -133,49 +117,39 @@ app.use(
   require('./lib/route/users')
 );
 
-app.use(
-  '/requests/',
-  require('./lib/route/requests')
-);
+app.use('/requests/', require('./lib/route/requests'));
 
-app.use(
-  '/audit/',
-  require('./lib/route/audit')
-);
+app.use('/audit/', require('./lib/route/audit'));
 
-app.use(
-  '/reports/',
-  require('./lib/route/reports')
-);
+app.use('/reports/', require('./lib/route/reports'));
 
 // catch 404
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.render('not_found');
 });
-
 
 // error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err,
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {},
+  });
 });
 
 module.exports = app;
